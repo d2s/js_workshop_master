8e56c547195590746812d76b315eab9cae0dbb17
Daniel Schildt <daniel@autiomaa.org>
2025-10-21 23:04:50 +0300

    feat(js): add initial WS3 Exercise 3 solution
    - Add initial WS3 Exercise 3 solution.
    - Still missing part of `textarea:focus` related features.
    - Form contents are not sent anywhere, but there is a bit of early validation.
    
    More details:
    
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/textarea
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/button
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/text
    - https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
    - https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/value
    - https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement#events
    - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
    - https://www.nngroup.com/articles/required-fields/
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/required
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/minlength
    - https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Attributes/maxlength
    - https://developer.mozilla.org/en-US/docs/Web/CSS/:focus
    - https://frontendmasters.com/blog/the-coyier-css-starter/

--- WS3-JavaScript-Events/ws3-scripts/ws3-ext3.js
+++ WS3-JavaScript-Events/ws3-scripts/ws3-ext3.js
@@ -1,5 +1,92 @@
-// --------------------------------------------------------
 /**
  * Output element for the functions.
  */
 const output = document.querySelector("#js-exerciseOutput");
+
+/**
+ * Shorter name for the form textarea element
+ */
+const exampleTextarea = document.getElementById("js-example-textarea");
+
+/**
+ * Reusable text output function.
+ *
+ * @param {String} outputTextContents
+ */
+function showOutputText(outputTextContents) {
+	console.log(outputTextContents);
+	// Add text to the HTML document's output section.
+	output.append(outputTextContents);
+}
+
+/**
+ * Action to run when mouse pointer enters element.
+ */
+function pointerEnterAction() {
+	const outputTextContents = "Stepped over my a mouse! ";
+	showOutputText(outputTextContents)
+}
+
+/**
+ * Action to run when mouse pointer leaves element.
+ */
+function pointerLeaveAction() {
+	const outputTextContents = "Bye bye mouse! ";
+	showOutputText(outputTextContents)
+}
+
+
+/**
+ * Form input textarea content lenght.
+ *
+ * - https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/value
+ */
+// let textareaValue = exampleTextarea.value;
+// let textareaCharacter = exampleTextarea.length;
+
+const textareaCharcount = document.getElementById("js-example-textarea-charcount-amount");
+
+/**
+ * Update exampleTextarea character count after keypress.
+ *
+ * - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+ * - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
+ */
+exampleTextarea.addEventListener("keyup", () => {
+  textareaCharcount.innerText = `${exampleTextarea.value.length} characters`;
+});
+
+/**
+ * Submit button
+ *
+ * - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+ * - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
+ */
+const sendButton = document.querySelector("#js-example-send-button");
+// const submitButton = document.querySelector("#js-example-submit");
+const warning = document.querySelector("#js-example-warning");
+const success = document.querySelector("#js-example-success");
+
+function checkFormContents() {
+	// Check that there are more than zero characters on the textarea contents.
+  if (exampleTextarea.value.length === 0) {
+		// Show warning message by removing hidden element state.
+    warning.hidden = false;
+		success.hidden = true;
+  } else {
+		// Hide warning message by adding hidden element state.
+    warning.hidden = true;
+		success.hidden = false;
+  }
+};
+
+sendButton.addEventListener("click", checkFormContents);
+
+
+
+// exampleTextarea.addEventListener("pointerenter", pointerEnterAction);
+// exampleTextarea.addEventListener("pointerleave", pointerLeaveAction);
+
+
+
+
