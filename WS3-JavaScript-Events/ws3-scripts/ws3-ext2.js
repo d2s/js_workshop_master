0e75131e0830c06af5252736ebcac2e1e20e13de
Daniel Schildt <daniel@autiomaa.org>
2025-10-21 17:45:19 +0300

    feat(js): add WS3 Exercise 2 solution
    - Add WS3 Exercise 2 solution.
    - Add more accessible output element.
    - Add reusable text output function.
    - Add event listeners to the headings.
    
    More details:
    
    - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
    - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
    - https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
    - https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent
    - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerover_event
    - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerenter_event
    - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerleave_event

--- WS3-JavaScript-Events/ws3-scripts/ws3-ext2.js
+++ WS3-JavaScript-Events/ws3-scripts/ws3-ext2.js
@@ -1,5 +1,50 @@
-// --------------------------------------------------------
 /**
  * Output element for the functions.
  */
 const output = document.querySelector("#js-exerciseOutput");
+
+/**
+ * Reusable text output function.
+ *
+ * @param {String} outputTextContents
+ */
+function showOutputText(outputTextContents) {
+	console.log(outputTextContents);
+	// Add text to the HTML document's output section.
+	output.append(outputTextContents);
+}
+
+/**
+ * Action to run when mouse pointer enters element.
+ */
+function pointerEnterAction() {
+	const outputTextContents = "Stepped over my a mouse! ";
+	showOutputText(outputTextContents)
+}
+
+/**
+ * Action to run when mouse pointer leaves element.
+ */
+function pointerLeaveAction() {
+	const outputTextContents = "Bye bye mouse! ";
+	showOutputText(outputTextContents)
+}
+
+// -----------------------------------------
+// Add event listeners
+//
+// - https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+// - https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Events
+// - https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent
+// - https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent
+// - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerover_event
+// - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerenter_event
+// - https://developer.mozilla.org/en-US/docs/Web/API/Element/pointerleave_event
+
+// Top level heading on the page header
+// document.querySelector("#js-headerHeading").addEventListener("pointerenter", pointerEnterAction);
+// document.querySelector("#js-headerHeading").addEventListener("pointerleave", pointerLeaveAction);
+
+// Exercise heading
+document.querySelector("#js-exerciseHeading").addEventListener("pointerenter", pointerEnterAction);
+document.querySelector("#js-exerciseHeading").addEventListener("pointerleave", pointerLeaveAction);
